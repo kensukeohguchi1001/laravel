a5919857281f2a6f324c6e90a8a11a5dc7fc6c7a
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-06-05 13:24:36 +0900

    controller validate

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,11 +8,17 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['data' => $request->data]);
+        return view('hello.index', ['msg' => 'フォームを入力']);
     }
 
     public function post(Request $request)
     {
-        return view('hello.index', ['msg' => $request->msg]);
+        $validate_rule = [
+            'name' => 'required',
+            'email' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+        $this->validate($request, $validate_rule);
+        return view('hello.index', ['msg' => '正しく入力されました']);
     }
 }
