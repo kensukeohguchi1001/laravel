67fe054a7c3fca435f43625a887e4f0dbcd91ce3
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-06-16 07:12:56 +0900

    検索とスコープについて①

--- app/Person.php
+++ app/Person.php
@@ -11,6 +11,22 @@ class Person extends Model
 
     public function getData()
     {
-        return $this->id . ':' . '$this->name' . '(' . $this->age . ')';
+        return $this->id . ':' . $this->name . '(' . $this->age . ')';
     }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
+    
 }
